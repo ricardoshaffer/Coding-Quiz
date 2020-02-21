b18e2a0f518157f71755c2b35b5c70fdece550d0
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-02-21 12:34:18 -0500

    added folder specifically for assets containing css and javascript.

---
+++ assets/question-objects.js
@@ -0,0 +1,28 @@
+let quizQuestions = [
+    {
+        question: "something?",
+        multiChoice: ["one", "two", "three", "four", "all of the above"],
+        correctAnswer: "one"
+    },
+    {
+        question: "something 2?",
+        multiChoice: ["one", "two", "three", "four", "all of the above"],
+        correctAnswer: "two"
+    },
+    {
+        question: "something 3?",
+        multiChoice: ["one", "two", "three", "four", "all of the above"],
+        correctAnswer: "three"
+    },
+    {
+        question: "something 4?",
+        multiChoice: ["one", "two", "three", "four", "all of the above"],
+        correctAnswer: "four"
+    },
+    {
+        question: "something 5?",
+        multiChoice: ["one", "two", "three", "four", "all of the above"],
+        correctAnswer: "all of the above"
+    },
+
+]
