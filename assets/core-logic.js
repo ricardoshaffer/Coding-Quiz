232460a6986364e2f3bbb7628f7caf3d85521f40
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-02-21 23:28:50 -0500

    edited timer to hide upon expiration or completion

--- assets/core-logic.js
+++ assets/core-logic.js
@@ -1,5 +1,4 @@
 
- //   $(document).ready(function() {
 // variables for data attribution and processing
 let currentQuestionNo= 0;
 let possibleAnswers = document.getElementById("myList");
@@ -10,7 +9,7 @@ let passedValue;
 let feedbackEl = document.getElementById("feedbackEl");
 document.getElementById("fading");
 
-let time = 75;
+let time = 50; //set at 10 seconds a question
 
 
 
@@ -28,66 +27,68 @@ function startQuiz() {
 
 function getQuestion() {
 
-    // get current question object from array
+// get current question object from array
     currentQuestion = jsQuizQuestions[currentQuestionNo];
-    // update title with current question
+// update title with current question
     let theQuestion = document.getElementById("Question");
     possibleAnswers.innerHTML = "";
     let questionNumber = ([currentQuestionNo + 1] + ". ");
     theQuestion.textContent = (questionNumber + currentQuestion.question);
     console.log(theQuestion.textContent);
-    // loop over choices
+// display question options
     currentQuestion.multiChoice.forEach(function(theChoice, i) {
-      // create new button for each choice
+// create button for each option
       let selectedAnswer = document.createElement("button");
       selectedAnswer.setAttribute("class", "list-group-item list-group-item-action");
       selectedAnswer.setAttribute("data-toggle", "list");
       selectedAnswer.setAttribute("value", theChoice);
       selectedAnswer.setAttribute("role", "tab");
+// display on the page
       selectedAnswer.textContent = i + 1 + ". " + theChoice;
-      // listening for a click on the answer
-      // display on the page
+// processing click on the answer
       selectedAnswer.onclick = getValue;
       possibleAnswers.appendChild(selectedAnswer);
       console.log("question Number: " + theChoice);
 
     });
-  
+// create actual question to push to getValue function
   passedValue = (jsQuizQuestions[currentQuestionNo].correctAnswer);
 
   ///// NEXT QUESTION FUNCTION
 
   function getValue() {
-  // check if user guessed wrong
+// if it's a wrong answer
   if (this.value !== passedValue) {
     console.log(jsQuizQuestions[currentQuestionNo].correctAnswer);
-    // penalize time
-    time -= 15;
-
+    time -= 10;
     if (time < 0) {
       time = 0;
     }
-
-    // display new time on page
+// display new time on page
     showTimer.textContent = time;
+// stylizing and presenting incorrect answer
     feedbackEl.textContent = "INCORRECT";
     console.log(jsQuizQuestions[currentQuestionNo].correctAnswer);
     feedbackEl.setAttribute("class", "p-3 mb-2 bg-danger text-white showing");
     feedbackEl.setAttribute("class", "p-3 mb-2 bg-danger text-white");
   } else {
+// stylizing and presenting correct answer
     feedbackEl.textContent = "YOU GOT IT!";
     feedbackEl.setAttribute("class", "p-3 mb-2 bg-success text-white showing");
     feedbackEl.setAttribute("class", "p-3 mb-2 bg-success text-white");
 
   };
-  
+// move on to the next question
   currentQuestionNo ++;
   if(currentQuestionNo < jsQuizQuestions.length){
   getQuestion();
   console.log("question length: " + jsQuizQuestions.length);
- 
+ // processes end of quiz
   } else {
-    feedbackEl.textContent = "YOU'RE DONE'!";
+    feedbackEl.setAttribute("class", "hiding");
+    showTimer.setAttribute =("class", "p-3 mb-2 bg-success text-white showing");
+    showTimer.textContent = "You're done!";
+    clearInterval(time);
   }
   }
  
@@ -96,16 +97,13 @@ function clockTick() {
   // update time
   time--;
   showTimer.textContent = time;
-
-  // check if user ran out of time
   if (time <= 0) {
     quizEnd();
   }
 }
 function quizEnd() {
   // stop timer
-  clearInterval(showTimer);
-
+  clearInterval(time);
   // show end screen
   var endScreenEl = document.getElementById("end-screen");
   endScreenEl.removeAttribute("class");
@@ -117,4 +115,3 @@ function quizEnd() {
   // hide questions section
   theQuestion.setAttribute("class", "hide");
 }
-//});
